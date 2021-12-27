2fdb867a86ef9390dac4e68fc32120c4e9f67834
Tran Chanh Tam <18521372@gm.uit.edu.vn>
2021-12-27 12:27:22 +0700

    m

---
+++ qltx/qltx/UserLogin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace qltx
+{
+    public class UserLogin
+    {
+        public string quyen { set; get; }
+        public string UserID { set; get; }
+        public string UserName {set; get;}
+    }
+}
